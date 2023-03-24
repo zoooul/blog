016549a841da252f0eaad5e53d5dc07f42f30358
Tommy <zoooul@protonmail.com>
2023-03-24 09:00:45 +0100

    install email_validator in forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms.validators import DataRequired, URL, Email,email_validator
 from flask_ckeditor import CKEditorField
 
 ##WTForm
